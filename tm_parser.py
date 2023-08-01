a21c7d05893c4fd5ade6eabd5e67da8668aa0239
anton_svyrydenko <antonsvyrydenko@gmail.com>
2023-08-01 09:57:55 +0300

    init

---
+++ tm_parser.py
@@ -0,0 +1,34 @@
+import sys
+import tyrone_mings
+
+input_file = sys.argv[1]  # 'argentina.txt'
+country = input_file.split('.')[0]
+
+# read file with clubs links
+with open(input_file, 'r') as input_file_source:
+    clubs = input_file_source.readlines()
+
+# process link by link
+for club in clubs:
+    # split str on CLUB and URL
+    club_data = club.split('__')
+    club_name = club_data[0]
+    club_url = club_data[1]
+
+    print(f'Processing {club_name}...')
+
+    player_urls = tyrone_mings.get_player_urls_from_club_page(club_url)
+    player_urls = [
+        player_url for player_url in player_urls if
+        'marktwertverlauf' not in player_url
+    ]
+
+    print(f'Fetched {len(player_urls)} players.')
+
+    with open(f'FIFA_{country}_2023.txt', 'a+') as output_file:
+        output_file.write(club_name + '\n\n')
+        for player_url in player_urls:
+            output_file.write(player_url + '\n')
+        output_file.write('\n\n')
+
+    print('Players data saved.\n')
