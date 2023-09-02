15eb28a64edc06ede70a9b085b9b2452928fc3bc
anton_svyrydenko <antonsvyrydenko@gmail.com>
2023-09-02 19:16:09 +0300

    feat: add config file. TODO: complete the ISO codes list

---
+++ config.py
@@ -0,0 +1,17 @@
+COUNTRY_ISO_CODE = {
+    'Albania': 'ALB1', 'Algeria': 'ALG1', 'Andorra': 'AND1',
+    'Argentina': 'AR1N', 'Armenia': 'ARM1', 'Australia': 'AUS1',
+    'Austria': 'A1', 'Azerbaijan': 'AZ1', 'Belgium': 'BE1',
+    'Bolivia': 'BO1A', 'Bosnia': 'BOS1', 'Brazil': 'BRA1',
+    'Bulgaria': 'BU1', 'Chile': 'CLPD', 'China': 'CSL',
+    'Colombia': 'COL1', 'Costa-Rica': 'CRPD', 
+    'Croatia': 'KR1', 'Cyprus': 'ZYP1', 'Czech': 'TS1',
+    'Denmark': 'DK1', 'Ecuador': 'EL1A',
+    'Egypt': 'EGY1', 'England': ['GB1', 'GB2', 'GB3', 'GB4'],
+    'Estonia': 'EST1', 'Faroe': 'FARO', 'Finland': 'FI1',
+    'France': ['FR1', 'FR2'], 'Georgia': 'GE1N',
+    'Germany': ['L1', 'L2'], 'Gibraltar': 'GI1',
+    'Greece': 'GR1', 'Honduras': 'HO1A',
+    'Hungary': 'UNG1', 'Iceland': 'IS1'
+    'Salvador': 'SL1A',
+}
